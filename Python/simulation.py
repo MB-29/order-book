432ebdbf1edc0611c4719f0711b0eaee3abc1c85
Matthieu Blanke <matthieu@MacBook-Pro-de-Matthieu-2.local>
2020-04-04 15:18:12 +0200

    Plot theoretical price curve

--- Python/simulation.py
+++ Python/simulation.py
@@ -4,49 +4,63 @@ import numpy as np
 from order_book import OrderBook
 
 # Model parameters
-dt = 1e-2
-lambd = 1e-1
+T = 1
+Nt = 20
+dt = T/float(Nt)
+lambd = 1
 nu = 1e-2
 D = 1
-
-book = OrderBook(dt, D=D, lambd=lambd, nu=nu)
+J = lambd * np.sqrt(D/float(nu))
+book = OrderBook(dt, D=D, lambd=lambd, nu=nu, resolution=100)
 
 # Price timeseries
-n = 200
-prices = np.zeros(n)
+prices = np.zeros(Nt)
+time_interval = np.linspace(0, T, num=Nt)
 
 # Metaorder intensity
-m0 = 1
-t_start, t_end = n/3, 2*n/3
+m0 = 5
+n_start, n_end = Nt//5, 2*Nt//3
+A_low = m0/(J*np.sqrt(np.pi))
+A_high = np.sqrt(2*m0/J)
 
 # Simulation
-for t in range(n):
+for n in range(Nt):
 
     # Plot evolution
     ymin, ymax = -1, 1
     plt.ylim((ymin, ymax))
     plt.plot(book.X, book.density, label='order density', linewidth=1)
-    plt.title(f'iteration {t}')
+    plt.title(f'iteration {n}')
 
     # Display best_ask/bid prices
-    # plt.vlines(book.best_ask, ymin, ymax, label='best ask', color='blue')
-    # plt.vlines(book.best_bid, ymin, ymax, label='best bid', color='yellow')
     plt.vlines(book.price, ymin, ymax, label='price', color='red', linewidth=1)
+    plt.hlines(0, -1, 1, color='black', linewidth=0.5, linestyle='dashed')
+    plt.vlines(book.best_ask, ymin, ymax, label='best ask', color='blue')
+    plt.vlines(book.best_bid, ymin, ymax, label='best bid', color='yellow')
+    plt.vlines(0, 0, m0*dt/book.dx, label='traded quantity', color='cyan')
+
     plt.legend()
     plt.pause(0.001)
 
     # Add a metaorder
-    book.mt = m0 if (t >= t_start and t <= t_end) else 0
+    book.mt = m0 if (n >= n_start and n <= n_end) else 0
     book.timestep()
     plt.cla()
 
-    prices[t] = book.price
+    prices[n] = book.price
+
 plt.close()
 
+p_th = A_low*np.sqrt(D*time_interval[0:n_end-n_start])
+plt.plot(time_interval[n_start:n_end],
+         p_th, label='Theoretical impact', lw=1, color='green')
+
+
 # Plot price evoution
-parameters_string = r''
 # plt.title(r'Price evolution with $\textrm{dt} = {{{}}}$'.format(dt))
-plt.title(r'd$t={{{0}}}$, $\lambda={{{1}}}$, $\nu={{{2}}}$, $D={{{3}}}$'.format(dt, lambd, nu, D))
-plt.plot(np.arange(n), prices, label='price evolution')
+# plt.title(r'd$t={{{0}}}$, $\lambda={{{1}}}$, $\nu={{{2}}}$, $D={{{3}}}$'.format(
+#     dt, lambd, nu, D))
+plt.title(r'$m_0={{{0}}}$, $J={{{1}}}$'.format(m0, round(J, 2)))
+plt.plot(time_interval, prices, label='price evolution')
 plt.legend()
 plt.show()
